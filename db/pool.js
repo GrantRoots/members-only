1f8ba6458eca54aa1289c24c75df41279d48277b
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2025-04-12 14:35:18 -0700

    chore: set up koyeb deployment

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const ROLE_NAME = process.env.ROLE_NAME;
-const ROLE_PASSWORD = process.env.ROLE_PASSWORD;
-module.exports = new Pool({
-  connectionString: `postgresql://${ROLE_NAME}:${ROLE_PASSWORD}@localhost:5432/users`,
-});
+const ENV = process.env.ENV || null;
+if (ENV === "dev") {
+  const ROLE_NAME = process.env.ROLE_NAME;
+  const ROLE_PASSWORD = process.env.ROLE_PASSWORD;
+  module.exports = new Pool({
+    connectionString: `postgresql://${ROLE_NAME}:${ROLE_PASSWORD}@localhost:5432/users`,
+  });
+} else {
+  const DATABASE_HOST = process.env.DATABASE_HOST;
+  const DATABASE_USER = process.env.DATABASE_USER;
+  const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+  const DATABASE_NAME = process.env.DATABASE_NAME;
+  module.exports = new Pool({
+    connectionString: `postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?sslmode=require`,
+  });
+}
--- db/populatedb.js
+++ db/populatedb.js
@@ -29,14 +29,31 @@ INSERT INTO messages (message, date, first_name, last_name)
 VALUES ('Hello this is a test message', '4/11/2025', 'FirstName', 'LastName');
 `;
 
-const ROLE_NAME = process.env.ROLE_NAME;
-const ROLE_PASSWORD = process.env.ROLE_PASSWORD;
-async function main() {
-  const client = new Client({
-    connectionString: `postgresql://${ROLE_NAME}:${ROLE_PASSWORD}@localhost:5432/users`,
-  });
-  await client.connect();
-  await client.query(SQL);
-  await client.end();
+const ENV = process.env.ENV || null;
+if (ENV === "dev") {
+  const ROLE_NAME = process.env.ROLE_NAME;
+  const ROLE_PASSWORD = process.env.ROLE_PASSWORD;
+  async function main() {
+    const client = new Client({
+      connectionString: `postgresql://${ROLE_NAME}:${ROLE_PASSWORD}@localhost:5432/users`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+  }
+  main();
+} else {
+  const DATABASE_HOST = process.env.DATABASE_HOST;
+  const DATABASE_USER = process.env.DATABASE_USER;
+  const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+  const DATABASE_NAME = process.env.DATABASE_NAME;
+  async function main() {
+    const client = new Client({
+      connectionString: `postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?sslmode=require`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+  }
+  main();
 }
-main();
